3ade3c365fdbc6dd53f92398f68e775627f8da70
pranayp1409 <pranaysp22hcompe@student.mes.ac.in>
2025-03-22 17:29:12 +0530

    new

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Settings = () => {
   const userSettings = {
-    username: "johndoe",
-    email: "john.doe@example.com",
+    username: "pranay",
+    email: "pranay@example.com",
     teamName: "Development Team",
     roleName: "Developer",
   };
